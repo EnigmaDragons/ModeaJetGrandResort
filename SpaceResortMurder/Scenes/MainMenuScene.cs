af99a45daa69dab1b3903cdab9e5c7aa75c6a773
SilasReinagel <silas.reinagel@gmail.com>
2018-03-10 13:13:34 -0700

    Created Button Factory and base JamScene. Closes #80. Closes #45

--- SpaceResortMurder/Scenes/MainMenuScene.cs
+++ SpaceResortMurder/Scenes/MainMenuScene.cs
@@ -1,71 +1,39 @@
 ﻿using Microsoft.Xna.Framework;
 using MonoDragons.Core.AudioSystem;
-using MonoDragons.Core.PhysicsEngine;
+using MonoDragons.Core.Engine;
 using MonoDragons.Core.Scenes;
 using MonoDragons.Core.UserInterface;
-using System;
-using MonoDragons.Core.Engine;
-using MonoDragons.Core.Inputs;
+using SpaceResortMurder.Style;
 
 namespace SpaceResortMurder.Scenes
 {
-    public class MainMenuScene : IScene
+    public sealed class MainMenuScene : JamScene
     {
-        private ImageTextButton _start;
-        private ImageTextButton _credits;
-        private ImageTextButton _options;
-        private ClickUI _clickUi;
-        
-        public void Init()
+        protected override void OnInit()
         {
-            GameObjects.InitIfNeeded();
-            Input.ClearTransientBindings();
-            Audio.PlayMusic("MainTheme");
             GameState.Instance = new GameState();
-            _clickUi = new ClickUI();
-            _start = new ImageTextButton(new Rectangle(120, 610, 240, 50), () =>
-                {
-                    Audio.PlaySound("MenuButtonPress");
-                    Scene.NavigateTo(GameObjects.RoomNames.BlackRoom);
-                }, "Start Game",
-                "UI/PixelButton", "UI/PixelButton-Hover", "UI/PixelButton-Press")
-                { TextColor = Color.Black };
-            _credits = new ImageTextButton(new Rectangle(120, 770, 240, 50), () =>
-                {
-                    Audio.PlaySound("MenuButtonPress");
-                    Scene.NavigateTo(GameObjects.CreditsSceneName);
-                }, "Credits", "UI/PixelButton", "UI/PixelButton-Hover", "UI/PixelButton-Press")
-                { TextColor = Color.Black };
-            _options = new ImageTextButton(new Rectangle(120, 690, 240, 50), () =>
-                {
-                    Audio.PlaySound("MenuButtonPress");
-                    GameState.Instance.CurrentLocation = "Main Menu";
-                    Scene.NavigateTo(GameObjects.OptionsSceneName);
-                },
-                "Options", "UI/PixelButton", "UI/PixelButton-Hover", "UI/PixelButton-Press")
-                { TextColor = Color.Black };
-            _clickUi.Add(_start);
-            _clickUi.Add(_credits);
-            _clickUi.Add(_options);
+            Audio.PlayMusic("MainTheme");
+
+            Add(UiButtons.Menu("Start Game", new Vector2(120, 610), () => Scene.NavigateTo(GameObjects.RoomNames.BlackRoom)));
+            Add(UiButtons.Menu("Credits", new Vector2(120, 770), () => Scene.NavigateTo(GameObjects.CreditsSceneName)));
+            Add(UiButtons.Menu("Options", new Vector2(120, 690), () =>
+            {
+                GameState.Instance.CurrentLocation = "Main Menu";
+                Scene.NavigateTo(GameObjects.OptionsSceneName);
+            }));
         }
 
-        public void Update(TimeSpan delta)
-        {
-            _clickUi.Update(delta);
-        }
-
-        public void Draw()
+        protected override void DrawBackground()
         {
             UI.FillScreen("UI/MainMenuBg");
             UI.Darken();
             World.Draw("characters/resort_manager_colored", new Vector2(700, 500));
             UI.DrawCenteredWithOffset("UI/Title", new Vector2(0, -150));
             UI.DrawCenteredWithOffset("UI/Copyright", new Vector2(261, 27), new Vector2(620, 412));
+        }
 
-            _start.Draw(Transform2.Zero);
-            _credits.Draw(Transform2.Zero);
-            _options.Draw(Transform2.Zero);
-
+        protected override void DrawForeground()
+        {
             UI.FillScreen("UI/MainMenuBorder");
         }
     }
---
+++ SpaceResortMurder/Style/UiButtons.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using MonoDragons.Core.AudioSystem;
+using MonoDragons.Core.UserInterface;
+
+namespace SpaceResortMurder.Style
+{
+    public static class UiButtons
+    {
+        public static VisualClickableUIElement Menu(string text, Vector2 position, Action onClick)
+        {
+            return new ImageTextButton(new Rectangle(position.ToPoint(), new Point(240, 50)), () =>
+                    {
+                        Audio.PlaySound("MenuButtonPress");
+                        onClick();
+                    }, 
+                    text,
+                    "UI/PixelButton", "UI/PixelButton-Hover", "UI/PixelButton-Press")
+                {TextColor = Color.Black};
+        }
+    }
+}
