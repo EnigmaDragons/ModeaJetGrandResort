f811f09128256d09d14f0fb9082d6543fb4eb21f
SilasReinagel <silas.reinagel@gmail.com>
2018-03-10 12:37:23 -0700

    Added Pixel-Style Button and put it in Main Menu. Closes #75

--- SpaceResortMurder/Style/UiStyle.cs
+++ SpaceResortMurder/Style/UiStyle.cs
@@ -5,8 +5,9 @@ namespace SpaceResortMurder.Style
 {
     public static class UiStyle
     {
-        public static Color ButtonGreen = new Color(new Color(8, 82, 10), 0.2f);
-        public static Color ButtonHoverGreen = new Color(new Color(15, 186, 19), 0.2f);
-        public static Color ButtonPressedGreen = new Color(new Color(29, 135, 32), 0.2f);
+        public static Color TextGreen = new Color(29, 145, 77);
+        public static Color ButtonGreen = new Color(new Color(37, 185, 99), 0.2f);
+        public static Color ButtonHoverGreen = new Color(new Color(10, 165, 75), 0.2f);
+        public static Color ButtonPressedGreen = new Color(new Color(0, 134, 23), 0.2f);
     }
 }
