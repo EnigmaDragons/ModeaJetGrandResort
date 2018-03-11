7f2beb589d24d42567e1337e944eeb4ea4ed3032
noah reinagel <noah.r9@gmail.com>
2018-03-10 18:46:52 -0700

    added clues, fixed invisible hud buttons

---
+++ SpaceResortMurder/Clues/Necronomicon.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+using MonoDragons.Core.PhysicsEngine;
+
+namespace SpaceResortMurder.Clues
+{
+    public sealed class Necronomicon : Clue
+    {
+        public Necronomicon() : base("Placeholder/necronomicon", new Transform2(new Vector2(600, 600), new Size2(90, 106)), new Size2(225, 265), () => {}, 
+            "This necronomicon feels super out of place for a sci-fi murder mystery", "lazy writers") {}
+    }
+}
