8683c37c316d68789d3c61967fb9e755a9309f32
Tim Reinagel <tmr2_handbell@yahoo.com>
2018-03-11 20:14:48 -0700

    ChatBoxes now allow you to choose the line spacing.

--- SpaceResortMurder/Dialogs/ChatBox.cs
+++ SpaceResortMurder/Dialogs/ChatBox.cs
@@ -6,20 +6,24 @@ using MonoDragons.Core.AudioSystem;
 using MonoDragons.Core.Engine;
 using MonoDragons.Core.PhysicsEngine;
 using MonoDragons.Core.UserInterface;
+using MonoDragons.Core.Common;
 
 namespace SpaceResortMurder.Dialogs
 {
     public class ChatBox : IVisual, IAutomaton
     {
+        public const int FONTSLINESPACING = -1;
         private readonly int _maxLineWidth;
         private readonly SpriteFont _spriteFont;
         private readonly double _millisToCharacter = 35;
         private string _currentlyDisplayedMessage;
+        private readonly int _lineSpacing;
         private string _messageToDisplay;
         private long _totalMessageTime;
 
-        public ChatBox(string message, int maxLineWidth, SpriteFont spriteFont)
+        public ChatBox(string message, int maxLineWidth, SpriteFont spriteFont, int lineSpacing = FONTSLINESPACING)
         {
+            _lineSpacing = lineSpacing == FONTSLINESPACING ? spriteFont.LineSpacing : lineSpacing;
             _spriteFont = spriteFont;
             _maxLineWidth = maxLineWidth;
             _currentlyDisplayedMessage = "";
@@ -57,7 +61,9 @@ namespace SpaceResortMurder.Dialogs
 
         public void Draw(Transform2 parentTransform)
         {
-            UI.DrawText(_currentlyDisplayedMessage, parentTransform.Location, Color.White);
+            _currentlyDisplayedMessage.Split('\n').ForEachIndex((l, i)
+                => UI.DrawText(l, new Vector2(parentTransform.Location.X, parentTransform.Location.Y + i * _lineSpacing ) , Color.White));
+            //UI.DrawText(_currentlyDisplayedMessage, parentTransform.Location, Color.White);
         }
 
         private string WrapText(string text)
